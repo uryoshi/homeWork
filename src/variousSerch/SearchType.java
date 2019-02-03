6448f235b2ff6aa01030d2f312cd0c5f314949bf
uryoshi <uryoshi@users.noreply.github.com>
2019-02-04 01:42:04 +0900

    javadocコメント修正

--- src/variousSerch/SearchType.java
+++ src/variousSerch/SearchType.java
@@ -1,14 +1,31 @@
 package variousSerch;
 
+/**
+ * 探索方式Enum
+ */
 public enum SearchType {
-    BINARY(1), LINER(2);
 
+    /** 二分探索 */
+    BINARY(1),
+
+    /** 線形探索 */
+    LINER(2);
+
+    /** 探索タイプ */
     private int type;
 
+    /**
+     * コンストラクタ。
+     */
     private SearchType(int type) {
     }
 
-    public int getId() {
+    /**
+     * 探索方式取得。
+     *
+     * @return 探索方式
+     */
+    public int getType() {
         return type;
     }
 }
